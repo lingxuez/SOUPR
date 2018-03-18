60a100d4206089f08c5793e4a3068a6c6cce2554
Lingxue Zhu <lingxue.zhu@gmail.com>
2018-03-18 16:48:59 -0400

    parallel cross validation

--- R/cvSOUP.R
+++ R/cvSOUP.R
@@ -1,43 +1,44 @@
 #' Cross Validation for SOUP
 #' 
+#' @param expr A cell-by-gene expression matrix, either the raw counts or log-transformed expressions. 
+#' @param type "log" if \code{expr} has been normalized and log-transformed (default),
+#'     or "count" (default) \code{expr} contains the raw counts.
+#' @param nfold Number of folds
+#' @param nCV Number of repetitions
+#' @param Ks A sequence of cluster numbers
+#' @param mc.cores Number of cores for parallelization
+#' @param verbose Whether to print progress
+#' 
 #' @export
 #' 
-cvSOUP <- function(expr, type="count", 
-                   nfold=10, nCV=10, Ks=c(2:10)) {
+cvSOUP <- function(expr, type="log", 
+                   nfold=10, nCV=10, Ks=c(2:10), mc.cores=10,
+                   verbose=TRUE) {
   
   cv.errors = matrix(NA, nrow=nCV, ncol=length(Ks))
   cv.sds = matrix(NA, nrow=nCV, ncol=length(Ks))
-  cv.aics = matrix(NA, nrow=nCV, ncol=length(Ks))
-  cv.bics = matrix(NA, nrow=nCV, ncol=length(Ks))
-  
+
   for (i.cv in c(1:nCV)) {
+    if (verbose) {
+      cat("CV", i.cv, "...")
+    }
     cv.out = cv.error.SOUP(expr=expr, type=type, 
                            nfold=nfold, Ks=Ks)
     cv.errors[i.cv, ] = cv.out$cvm
     cv.sds[i.cv, ] = cv.out$cvsd
-    cv.aics[i.cv, ] = cv.out$cvaic
-    cv.bics[i.cv, ] = cv.out$cvbic
+  }
+  
+  if (verbose) {
+    cat("done.\n")
   }
   
   cvm = colMeans(cv.errors)
-  cvaic = colMeans(cv.aics)
-  cvbic = colMeans(cv.bics)
-  
-  K.aic = Ks[which.min(cvaic)]
-  K.bic = Ks[which.min(cvbic)]
   K.cv = Ks[which.min(cvm)]
-  K.cv.each = Ks[apply(cv.errors, 1, which.min)]
-  K.majority = names(sort(table(K.cv.each), decreasing = TRUE)[1])
-  
+
   return(list(cv.errors = cv.errors,
+              cv.sds = cv.sds,
               cvm = cvm,
-              cvsd = colMeans(cv.sds),
-              cvaic = cvaic,
-              cvbic = cvbic,
-              K.cv = Ks[which.min(cvm)],
-              K.aic = K.aic,
-              K.bic = K.bic,
-              K.majority=K.majority))
+              K.cv = K.cv))
 }
 
 #' Compute Cross Validation Errors
@@ -47,53 +48,60 @@ cvSOUP <- function(expr, type="count",
 #'     or "count" (default) \code{expr} contains the raw counts.
 #' @param nfold Number of folds
 #' @param Ks A sequence of cluster numbers
+#' @param seed (optional) random seed
+#' @param mc.cores Number of cores for parallelization
 #' 
 #' @export
 cv.error.SOUP <- function(expr, type="log", 
-                          nfold=10, Ks=c(2:10), seed=NULL) {
-  n.sc = nrow(expr)
-  cv.error = matrix(0, nrow=nfold, ncol=length(Ks))
-  cv.aic = matrix(0, nrow=nfold, ncol=length(Ks))
-  cv.bic = matrix(0, nrow=nfold, ncol=length(Ks))
+                          nfold=10, Ks=c(2:10), seed=NULL,
+                          mc.cores=2) {
+  
+  ## cross validation
+  doCV <- function(fold, nfold, i.permute.ind, 
+                   expr, Ks, type) {
+    n.sc = nrow(expr)
+    n.K = length(Ks)
+    fold.size = floor(n.sc / nfold)
+    
+    ## test set
+    i.start = (fold.size*(fold-1)+1)
+    i.end = min(fold.size*fold, n.sc)
+    i.test = i.permute.ind[c(i.start:i.end)]
+    n.test = length(i.test)
+    
+    ## SOUP on training set
+    soup.out = SOUP(expr=expr[-i.test, ], Ks=Ks, type=type,
+                    nstart=50, verbose=FALSE)
+    
+    ## Predict on test set
+    cv.error = rep(NA, n.K)
+    for (i.K in c(1:n.K)) {
+      train.centers = soup.out$centers[[i.K]]
+      test.theta = predictTheta(new.expr=expr[i.test, ], 
+                                t.centers=t(train.centers))
+      test.pred = test.theta %*% train.centers
+      cv.error[i.K] = sum((expr[i.test, ] - test.pred)^2) / n.test
+    }
+    
+    return(cv.error)
+  }
   
   ## permute indices
+  n.sc = nrow(expr)
   if (!is.null(seed)) {
     set.seed(seed)
   }
   i.permute.ind = base::sample(c(1:n.sc), size=n.sc, replace=FALSE)
-  fold.size = floor(n.sc / nfold)
   
-  for (fold in c(1:nfold)) {
-    if (fold < nfold) {
-      i.test = c( (fold.size*(fold-1)+1) : (fold.size*fold))
-    } else {
-      i.test = c((fold.size*(fold-1)+1) : n.sc)
-    }
-    i.train = c(1:n.sc)[-i.test]
-    n.test = length(i.test)
-    
-    ## SOUP on training set
-    soup.out = SOUP(expr[i.train, ], Ks=Ks, type=type)
-    
-    ## Predict remaining
-    for (i.K in c(1:length(Ks))) {
-      train.centers = soup.out$centers[[i.K]]
-      test.theta = predictTheta(expr[i.test, ], 
-                                t.centers=t(train.centers))
-      test.pred = test.theta %*% train.centers
-      cv.error[fold, i.K] = sum((expr[i.test, ] - test.pred)^2) / n.test
-    }
-    
-    ## AIC = 2K + n*ln(2 RSS)
-    cv.aic[fold, ] = 2 * Ks + n.test * log(2 * cv.error[fold, ])
-    ## BIC
-    cv.bic[fold, ] = log(n.test) * Ks + n.test * log(2 * cv.error[fold, ])
-  }
+  ## cross validation in parallel
+  cv.errors = parallel::mclapply(c(1:nfold), FUN=doCV, 
+                                   nfold=nfold, i.permute.ind=i.permute.ind, 
+                                   expr=expr, Ks=Ks, type=type,
+                                   mc.cores=mc.cores, mc.preschedule=TRUE)
+  cv.errors = matrix(unlist(cv.errors), nrow=nfold, byrow=TRUE)
   
-  return(list(cvm = colMeans(cv.error),
-              cvsd = apply(cv.error, 2, sd),
-              cvaic = colMeans(cv.aic),
-              cvbic = colMeans(cv.bic)
+  return(list(cvm = colMeans(cv.errors),
+              cvsd = apply(cv.errors, 2, sd)
   ))
 }
 
